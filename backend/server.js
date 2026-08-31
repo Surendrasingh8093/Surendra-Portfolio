3a1c73e7be0adabdb47adfa62cfc259902cd24d5
Surendrasingh8093 <surendras11884@gmail.com>
2026-09-01 05:21:47 +0530

    update to server.js file

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,15 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: [
+      "http://localhost:5173",
+      "https://YOUR-FRONTEND.vercel.app",
+    ],
+    credentials: true,
+  }
+));
 
 app.use(express.json({
   limit: "10mb"
@@ -34,6 +42,12 @@ app.use(
 app.use("/api/contact", require("./routes/contactRoutes"));
 app.use("/api/projects", require("./routes/projectRoutes"));
 app.use("/api/documents", require("./routes/documentRoutes"));
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Surendra Portfolio API is running",
+  });
+});
 
 const PORT = process.env.PORT || 5000;
 
